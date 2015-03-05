0546b8337cf828bd75e8e433b1f998b9e24e8fbe
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-03-05 11:41:44 +0900

    First import

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var s = flag.String("s", "hikari", "speaker")
+
+func say() int {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return 1
+	}
+
+	home := os.Getenv("HOME")
+	if runtime.GOOS == "windows" {
+		home = os.Getenv("USERPROFILE")
+	}
+
+	fn := filepath.Join(home, ".voicetext-apikey")
+	b, err := ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err, fn)
+		return 1
+	}
+	apikey := strings.TrimSpace(string(b))
+
+	params := url.Values{}
+	params.Set("text", strings.Join(flag.Args(), " "))
+	req, err := http.NewRequest("POST", "https://api.voicetext.jp/v1/tts", strings.NewReader(params.Encode()))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err)
+		return 1
+	}
+	req.SetBasicAuth(apikey, "")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err)
+		return 1
+	}
+
+	f, err := ioutil.TempFile(os.TempDir(), "say")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err)
+		os.Exit(1)
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	_, err = io.Copy(f, res.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err)
+		return 1
+	}
+
+	err = play(f.Name())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "say", err)
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(say())
+}
---
+++ play_windows.go
@@ -0,0 +1,30 @@
+// +build windows
+
+package main
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+const (
+	SND_SYNC      = 0x0000
+	SND_NODEFAULT = 0x0002
+	SND_FILENAME  = 0x00020000
+)
+
+var (
+	libwinmm      = syscall.NewLazyDLL("winmm")
+	procPlaySound = libwinmm.NewProc("PlaySoundW")
+)
+
+func play(filename string) error {
+	r0, _, err := procPlaySound.Call(
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(filename))),
+		0,
+		SND_SYNC|SND_NODEFAULT|SND_FILENAME)
+	if r0 == 0 {
+		return err
+	}
+	return nil
+}
