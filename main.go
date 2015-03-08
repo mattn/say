a93f6793426627981fc224c02cd9ffb3cf664599
mattn <mattn.jp@gmail.com>
2015-03-08 21:59:45 +0900

    Add missing flags that should be implemented

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -13,7 +14,18 @@ import (
 	"strings"
 )
 
-var speaker = flag.String("s", "hikari", "speaker")
+var speaker = flag.String("speaker", "hikari", "show/haruka/hikari/takeru/santa/bear")
+var emotion = flag.String("emotion", "", "happiness/anger/sadness")
+var emotion_level = flag.Int("emotion_level", 1, "1/2")
+var pitch = flag.Int("pitch", 100, "50% - 200%")
+var speed = flag.Int("speed", 100, "50% - 400%")
+var volume = flag.Int("volume", 100, "50% - 200%")
+
+type response struct {
+	Error struct {
+		Message string `json:"message"`
+	} `json:"error"`
+}
 
 func say() int {
 	flag.Parse()
@@ -37,6 +49,13 @@ func say() int {
 	params := url.Values{}
 	params.Set("text", strings.Join(flag.Args(), " "))
 	params.Set("speaker", *speaker)
+	if *emotion != "" {
+		params.Set("emotion", *emotion)
+		params.Set("emotion_level", fmt.Sprint(*emotion_level))
+	}
+	params.Set("pitch", fmt.Sprint(*pitch))
+	params.Set("speed", fmt.Sprint(*speed))
+	params.Set("volume", fmt.Sprint(*volume))
 	req, err := http.NewRequest("POST", "https://api.voicetext.jp/v1/tts", strings.NewReader(params.Encode()))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "say", err)
@@ -47,6 +66,16 @@ func say() int {
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "say", err)
+		return 1
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		var resp response
+		if err = json.NewDecoder(res.Body).Decode(&resp); err == nil {
+			fmt.Fprintln(os.Stderr, "say", resp.Error.Message)
+		} else {
+			fmt.Fprintln(os.Stderr, "say", "something wrong")
+		}
 		return 1
 	}
 
